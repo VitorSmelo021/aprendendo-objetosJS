f514345cfb70886d3c73b0a67bddaa9414db7d80
Vitor Samuel <108810741+VitorSmelo021@users.noreply.github.com>
2023-04-19 12:41:41 -0300

    Add files via upload

---
+++ Praticando-objt/assunto/conteudo.js
@@ -0,0 +1,15 @@
+
+
+
+const user = {
+    id: 1,
+    name: "Vitor Samuel",
+    age: 18,
+    city: "Maceió",
+
+    function: () => {
+        return console.log(`Usuário: ${user.name}`)
+    }
+};
+
+user.function();
